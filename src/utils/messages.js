0bc96ecd505ce2ba6e8fe78cbac6a9a554c70436
Znacker <yakuzatv0@gmail.com>
2025-04-07 02:09:55 -0300

    first commit

---
+++ src/utils/messages.js
@@ -0,0 +1,80 @@
+/**
+ * Mensagens do bot
+ *
+ * @author Dev Gui
+ */
+const { BOT_NAME, PREFIX } = require("../config");
+
+exports.waitMessage = "Carregando dados...";
+
+exports.menuMessage = () => {
+  const date = new Date();
+
+  return `╭━━⪩ BEM VINDO! ⪨━━
+▢
+▢ • ${BOT_NAME}
+▢ • Data: ${date.toLocaleDateString("pt-br")}
+▢ • Hora: ${date.toLocaleTimeString("pt-br")}
+▢ • Prefixo: ${PREFIX}
+▢
+╰━━─「🪐」─━━
+
+╭━━⪩ DONO ⪨━━
+▢
+▢ • ${PREFIX}off
+▢ • ${PREFIX}on
+▢
+╰━━─「🌌」─━━
+
+╭━━⪩ ADMINS ⪨━━
+▢
+▢ • ${PREFIX}anti-link (1/0)
+▢ • ${PREFIX}auto-responder (1/0)
+▢ • ${PREFIX}ban
+▢ • ${PREFIX}exit (1/0)
+▢ • ${PREFIX}hidetag
+▢ • ${PREFIX}welcome (1/0)
+▢
+╰━━─「⭐」─━━
+
+╭━━⪩ PRINCIPAL ⪨━━
+▢
+▢ • ${PREFIX}attp
+▢ • ${PREFIX}cep
+▢ • ${PREFIX}google-search
+▢ • ${PREFIX}ping
+▢ • ${PREFIX}sticker
+▢ • ${PREFIX}to-image
+▢ • ${PREFIX}ttp
+▢ • ${PREFIX}yt-search
+▢
+╰━━─「🚀」─━━
+
+╭━━⪩ DOWNLOADS ⪨━━
+▢
+▢ • ${PREFIX}play-audio
+▢ • ${PREFIX}play-video
+▢ • ${PREFIX}tik-tok
+▢ • ${PREFIX}yt-mp3
+▢ • ${PREFIX}yt-mp4
+▢
+╰━━─「🎶」─━━
+
+╭━━⪩ IA ⪨━━
+▢
+▢ • ${PREFIX}gpt-4
+▢ • ${PREFIX}ia-sticker
+▢ • ${PREFIX}pixart
+▢ • ${PREFIX}stablle-diffusion-turbo
+▢
+╰━━─「🚀」─━━
+
+╭━━⪩ CANVAS ⪨━━
+▢
+▢ • ${PREFIX}bolsonaro
+▢ • ${PREFIX}cadeia
+▢ • ${PREFIX}inverter
+▢ • ${PREFIX}rip
+▢
+╰━━─「❇」─━━`;
+};
